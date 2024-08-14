955045ccfc39d47febb770fd6b81f53eb193353a
hanzalahwaheed <hanzalah.w@gmail.com>
2024-08-15 01:49:12 +0530

    revert to non-rsc

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,24 @@
 "use client";
 
-import { type CoreMessage } from "ai";
-import { useState } from "react";
-import { continueConversation } from "./actions";
-import { readStreamableValue } from "ai/rsc";
-
-// Allow streaming responses up to 30 seconds
-export const maxDuration = 30;
+import { useChat } from "ai/react";
 
 export default function Chat() {
-  const [messages, setMessages] = useState<CoreMessage[]>([]);
-  const [input, setInput] = useState("");
-  const [data, setData] = useState<any>();
-
-  const onSubmit = async (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-    const newMessages: CoreMessage[] = [
-      ...messages,
-      { content: input, role: "user" },
-    ];
-
-    setMessages(newMessages);
-    setInput("");
-
-    const result = await continueConversation(newMessages);
-    setData(result.data);
-
-    for await (const content of readStreamableValue(result.message)) {
-      setMessages([
-        ...newMessages,
-        {
-          role: "assistant",
-          content: content as string,
-        },
-      ]);
-    }
-  };
-
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
-      {messages.map((m, i) => (
-        <div key={i} className="whitespace-pre-wrap">
+      {messages.map((m) => (
+        <div key={m.id} className="whitespace-pre-wrap">
           {m.role === "user" ? "User: " : "AI: "}
-          {m.content as string}
+          {m.content}
         </div>
       ))}
 
-      <form onSubmit={onSubmit}>
+      <form onSubmit={handleSubmit}>
         <input
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl text-black"
           value={input}
           placeholder="Say something..."
-          onChange={(e) => setInput(e.target.value)}
+          onChange={handleInputChange}
         />
       </form>
     </div>
